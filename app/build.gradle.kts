1085a918dc41b571f851d2a5e0e5ad9c2b38972d
Saya <sh5155@gmail.com>
2023-08-14 14:30:48 +1200

    One crumb of progress
    appconfiguration building in an android context. no tests.
    had to get rid of modules file, still not really sure why but it wouldn't
    compile otherwise (something about it not finding androidx.appcompat)

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.azure.data.appconfiguration"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.azure.data.appconfiguration"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    packaging {
+        resources.excludes.add("META-INF/*")
+    }
+}
+
+dependencies {
+    implementation("com.azure:azure-core:1.42.0")
+    implementation("com.azure:azure-json:1.1.0")
+    implementation("com.azure:azure-core-http-netty:1.13.6")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/azure/data/appconfiguration/MainActivity.java
@@ -0,0 +1,22 @@
+package com.azure.data.appconfiguration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.azure.data.appconfiguration.implementation.ClientConstants;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String randomStringFromAppConfig = ClientConstants.APP_CONFIG_TRACING_NAMESPACE_VALUE;
+
+        TextView text = findViewById(R.id.mainText);
+        text.setText(randomStringFromAppConfig);
+    }
+}
