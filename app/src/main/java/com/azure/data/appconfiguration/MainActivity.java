89856c17536fa580ebd4ba85ef9a3b5896af128b
Jamie Richards <jr1000000@gmail.com>
2023-08-29 21:18:45 +1200

    Initial HelloWorld Implementation
    Hacky solution, not functioning correctly due to failure to connect to socket.

--- app/src/main/java/com/azure/data/appconfiguration/MainActivity.java
+++ app/src/main/java/com/azure/data/appconfiguration/MainActivity.java
@@ -3,9 +3,11 @@ package com.azure.data.appconfiguration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.azure.data.appconfiguration.implementation.ClientConstants;
+import com.azure.data.appconfiguration.samples.appconfiguration.HelloWorld;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Proving that the app is interacting with appconfiguration
         String randomStringFromAppConfig = ClientConstants.APP_CONFIG_TRACING_NAMESPACE_VALUE;
+        HelloWorld.main();
         TextView text = findViewById(R.id.mainText);
         text.setText(randomStringFromAppConfig);
+        Log.i("Tests", randomStringFromAppConfig);
     }
 }
